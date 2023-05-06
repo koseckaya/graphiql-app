918bd8d6375fa7200b8c283e0274c72f951184e0
koseckaya <kosetsaya@gmail.com>
2023-05-06 20:51:21 +0300

    feat: change main

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -6,9 +6,9 @@ import Header from '@/components/Header';
 const Main = (props: { meta: ReactNode; children: ReactNode }) => (
   <div className="relative flex h-full w-full flex-col justify-between text-white antialiased">
     {props.meta}
-    
+
     <Header />
-    
+
     <main className="flex-auto py-5 text-xl">{props.children}</main>
 
     <Footer />
