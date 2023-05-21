7461b0b5dfd09e7170aeea96508cb1477165ad52
zhannach <zhanna.chaykovska@gmail.com>
2023-05-21 13:49:39 +0300

    feat: persist redux state on refresh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,22 @@ import '../firebase';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import Loader from '@/components/Loader';
 
 import { wrapper } from '../rtk/store';
 
 const MyApp = ({ Component, ...rest }: AppProps) => {
   const { store, props } = wrapper.useWrappedStore(rest);
+  const persistor = persistStore(store);
   const { pageProps } = props;
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 };
--- src/rtk/store.ts
+++ src/rtk/store.ts
@@ -1,9 +1,27 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { api } from './apiSlice';
 import { dataSlice } from './dataSlice';
 import { userSlice } from './userSlice';
+
+const persistConfig = {
+  type: REGISTER,
+  key: 'root',
+  storage,
+  timeout: 1000,
+  whitelist: [userSlice.name],
+};
 
 const reducers = {
   [dataSlice.name]: dataSlice.reducer,
@@ -13,12 +31,18 @@ const reducers = {
 
 const reducer = combineReducers(reducers);
 
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const makeStore = () =>
   configureStore({
-    reducer,
+    reducer: persistedReducer,
     devTools: true,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(api.middleware),
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(api.middleware),
   });
 
 type AppStore = ReturnType<typeof makeStore>;
